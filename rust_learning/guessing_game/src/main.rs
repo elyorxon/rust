b709397d40fcb5746ec14927d3b3593854c47b41
elyorxon <68835686+elyorxon@users.noreply.github.com>
2024-07-13 20:16:38 +0500

    added rand

--- rust_learning/guessing_game/src/main.rs
+++ rust_learning/guessing_game/src/main.rs
@@ -1,15 +1,21 @@
 use std::io;
+use rand::Rng;
+
 
 fn main (){
     println!("Raqamni taxmin qiling!");
 
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("Yashirin raqam:  {secret_number}");
+
     println!("Iltimos, o'ylagan raqamingizni kiriting.");
 
-    let mut guess = String::new();
+    let mut guess = String::new(); // mutable variable
 
     io::stdin()
         .read_line(&mut guess)
-        .expect("Failed to read line");
+        .expect("Qatorni o'qishda xatolik.");
 
     println!("Siz taxmin qilgan son: {guess}.");
 }
