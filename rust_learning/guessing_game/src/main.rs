09b2681c89a95cecb90a96ebb0cf74c929a78694
elyorxon <68835686+elyorxon@users.noreply.github.com>
2024-07-13 21:10:09 +0500

    added loop

--- rust_learning/guessing_game/src/main.rs
+++ rust_learning/guessing_game/src/main.rs
@@ -2,28 +2,30 @@ use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
 
-fn main (){
+fn main () {
     println!("Raqamni taxmin qiling!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
     println!("Yashirin raqam:  {secret_number}");
 
-    println!("Iltimos, o'ylagan raqamingizni kiriting.");
+    loop {
+        println!("Iltimos, o'ylagan raqamingizni kiriting.");
 
-    let mut guess = String::new(); // mutable variable
+        let mut guess = String::new(); // mutable variable
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Qatorni o'qishda xatolik.");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Qatorni o'qishda xatolik.");
 
-    let guess: u32 = guess.trim().parse().expect("Iltimos, son kiriting.");
+        let guess: u32 = guess.trim().parse().expect("Iltimos, son kiriting.");
 
-    println!("Siz taxmin qilgan son: {guess}.");
+        println!("Siz taxmin qilgan son: {guess}.");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Kichikroq son o'yladiz!"),
-        Ordering::Greater => println!("Kattaroq son o'yladiz!"),
-        Ordering::Equal => println!("Tabriklaymiz. To'g'ri raqamni todingiz!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Kichikroq son o'yladiz!"),
+            Ordering::Greater => println!("Kattaroq son o'yladiz!"),
+            Ordering::Equal => println!("Tabriklaymiz. To'g'ri raqamni topdingiz!"),
+           }
     }
 }
